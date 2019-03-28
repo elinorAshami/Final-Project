5b3cef1f5a02def95507ecca988b30c4d01238ae
elinorAshami <ashami0104@gmail.com>
2019-03-28 13:51:15 +0200

    Add files via upload

---
+++ includes/menu.js
@@ -0,0 +1,7 @@
+window.onload=function(){	
+	var body = document.body,
+    html = document.documentElement;
+	var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+	var Menu = document.getElementById("menu");
+	Menu.style.height(height);                      
+};

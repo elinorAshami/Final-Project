07e34c73097d971fc8cad3813c5e94d22ea97d69
gadyez <gady@payboxapp.com>
2019-04-13 20:07:03 +0300

    Add Channels

--- client/src/components/studio.js
+++ client/src/components/studio.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import '../App.css';
 import Button from '@material-ui/core/Button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlay,faStop,faStepBackward,faStepForward,faMicrophone,faMusic } from '@fortawesome/free-solid-svg-icons'
+import { faPlay,faStop,faStepBackward,faStepForward,faMicrophone,faMusic,faVolumeMute, faHeadphones } from '@fortawesome/free-solid-svg-icons'
 import Slider from '@material-ui/lab/Slider';
 
 class Studio extends Component {
@@ -56,22 +56,42 @@ class Studio extends Component {
                 </div>
                     <div className="channels">
                         <div className="channel">
-                            <div className="channel-icon">
-                                <img src="./images/guitar.png"/>
-                            </div>
+                            <img src="./images/guitar.png"/>
                             <div className="channel-details">
                                 <p>Gady Guitar</p>
-                                <Button className="channel-button">FX</Button>
-                                <Button className="channel-button">FX</Button>
-                                <Slider classes={{root: "volume-slider"}}
+                                <FontAwesomeIcon icon={faVolumeMute}/>
+                                <FontAwesomeIcon icon={faHeadphones}/>
+                                <Slider classes={{root: "volume-slider",track: "volume-track"}}
                                         value={this.state.volume}
                                         aria-labelledby="label"
                                         onChange={this.changeVolume}/>
-                                <Button className="channel-button">FX</Button>
+                                <span>FX</span>
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <img src="./images/guitar.png"/>
+                            <div className="channel-details">
+                                <p>Elinor Guitar</p>
+                                <FontAwesomeIcon icon={faVolumeMute}/>
+                                <FontAwesomeIcon icon={faHeadphones}/>
+                                <Slider classes={{root: "volume-slider",track: "volume-track"}}
+                                        value={this.state.volume}
+                                        aria-labelledby="label"
+                                        onChange={this.changeVolume}/>
+                                <span>FX</span>
                             </div>
                         </div>
                     </div>
-                    <div className="timeline">
+                    <div className="timeline-container">
+                        <div className="timeline">
+                            <div className="timebar">
+                            </div>
+                            <div className="timebar-channel">
+                            </div>
+                            <div className="timebar-channel">
+                            </div>
+                        </div>
+
                     </div>
             </div>
         );

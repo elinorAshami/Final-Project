a81408ea0b07f71c3cc0452ffde569464dc9701c
elinorAshami <ashami0104@gmail.com>
2019-04-24 10:07:50 +0300

    1 commit

--- app-new/src/app/app-routing.module.ts
+++ app-new/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {IndexComponent } from "./index/index.component";
+import { BandComponent} from "./band/band.component";
+
+const routes: Routes = [
+  { path: '' , redirectTo: '/index' , pathMatch:'full'},
+  { path: 'index' , component: IndexComponent },
+  { path: 'band' , component: BandComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app-new/src/app/app.module.ts
+++ app-new/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { BandComponent } from './band/band.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    BandComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,

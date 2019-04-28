bb21ed14b583e0cb1970575fe2b3c5838db96497
elinorAshami <ashami0104@gmail.com>
2019-04-28 12:22:59 +0300

    28.4 commit

--- app-new/src/app/app-routing.module.ts
+++ app-new/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {IndexComponent } from "./index/index.component";
-import { BandComponent} from "./band/band.component";
+import { IndexComponent } from "./index/index.component";
+import { BandComponent } from "./band/band.component";
 
 const routes: Routes = [
   { path: '' , redirectTo: '/index' , pathMatch:'full'},
   { path: 'index' , component: IndexComponent },
-  { path: 'band' , component: BandComponent }
+  { path: 'band' , component: BandComponent },
 ];
 
 @NgModule({

1f92507502ef149d6b14fbfb42d9471232d9925e
elinorAshami <ashami0104@gmail.com>
2019-04-28 16:22:39 +0300

    links works

---
+++ app-new/src/app/favoriets/favoriets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-favoriets',
+  templateUrl: './favoriets.component.html',
+  styleUrls: ['./favoriets.component.css']
+})
+export class FavorietsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ app-new/src/app/new-band/new-band.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-band',
+  templateUrl: './new-band.component.html',
+  styleUrls: ['./new-band.component.css']
+})
+export class NewBandComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
